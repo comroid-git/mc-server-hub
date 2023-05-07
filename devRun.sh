25bc9a807d9abfbe404cd0093990b4512bcc13fb
burdoto <kaleidox@comroid.org>
2023-05-07 02:46:05 +0200

    Update devRun.sh

--- devRun.sh
+++ devRun.sh
@@ -25,7 +25,7 @@ if [ "$variant" == "dev" ]; then
   if [ -f ".slave" ]; then
     debugArgs="-agentlib:jdwp=transport=dt_socket,server=n,address=dev.kaleidox.de:5005,suspend=y,onuncaught=y"
   else debugArgs="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005"; fi
-  DEV="true" $exec --no-daemon bootRun -Dorg.gradle.jvmargs="-Xdebug -XX:+HeapDumpOnOutOfMemoryError $debugArgs"
+  DEV="true" $exec --no-daemon bootRun -DDEBUG="true" -Dorg.gradle.jvmargs="-Xdebug -XX:+HeapDumpOnOutOfMemoryError $debugArgs"
 else
   # switch to production variant
   sudo systemctl enable mcsd-web --now
