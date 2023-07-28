03474568b055a23047959dc9a2869f3c2ed4b4af
burdoto <kaleidox@comroid.org>
2023-07-29 01:27:45 +0200

    change colors

--- src/api/main/java/org/comroid/mcsd/api/model/Status.java
+++ src/api/main/java/org/comroid/mcsd/api/model/Status.java
@@ -2,16 +2,17 @@ package org.comroid.mcsd.api.model;
 
 import lombok.Getter;
 import org.comroid.api.IntegerAttribute;
+import org.comroid.mcsd.util.McFormatCode;
 
 import java.awt.*;
 
 @Getter
 public enum Status implements IntegerAttribute {
-    Unknown     ("❔", Color.BLACK),
-    Offline     ("❌", Color.RED),
-    Starting    ("⏯", Color.BLUE),
-    Maintenance ("�", Color.MAGENTA),
-    Online      ("✅", Color.GREEN);
+    Unknown     ("❔", McFormatCode.DarkGray.getColor()),
+    Offline     ("❌", McFormatCode.Red.getColor()),
+    Starting    ("⏯", McFormatCode.Aqua.getColor()),
+    Maintenance ("�", McFormatCode.Yellow.getColor()),
+    Online      ("✅", McFormatCode.DarkGreen.getColor());
 
     private final String emoji;
     private final Color color;
