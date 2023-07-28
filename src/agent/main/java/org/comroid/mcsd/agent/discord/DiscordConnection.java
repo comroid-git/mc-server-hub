86c37ea742c9ca66e5034cae8d814a38e531e2e7
burdoto <kaleidox@comroid.org>
2023-07-28 10:31:28 +0200

    add dc->mc support

--- src/agent/main/java/org/comroid/mcsd/agent/discord/DiscordConnection.java
+++ src/agent/main/java/org/comroid/mcsd/agent/discord/DiscordConnection.java
@@ -9,21 +9,26 @@ import net.dv8tion.jda.api.entities.Webhook;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import org.comroid.api.*;
 import org.comroid.mcsd.agent.ServerProcess;
+import org.comroid.mcsd.core.entity.MinecraftProfile;
 import org.comroid.mcsd.core.repo.DiscordBotRepo;
 import org.comroid.mcsd.core.repo.MinecraftProfileRepo;
+import org.intellij.lang.annotations.Language;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.PrintStream;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.function.BiConsumer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 import static org.comroid.mcsd.core.util.ApplicationContextProvider.bean;
 
 @Data
 public class DiscordConnection extends Container.Base {
     public static final Pattern ChatPattern_Vanilla = Pattern.compile("INFO]:\\s<(?<username>[\\w-_]+)>\\s(?<message>.+)\\n"); //todo
+    private final BiConsumer<MinecraftProfile, String> chatRelay;
     private final DiscordAdapter adapter;
     private final ServerProcess srv;
 
@@ -34,15 +39,28 @@ public class DiscordConnection extends Container.Base {
                 .findById(Objects.requireNonNull(srv.getServer().getDiscordBot()))
                 .map(srv.getRunner()::adapter)
                 .orElseThrow();
-        final var publicChannelId = Optional.ofNullable(server.getPublicChannelId());
-        final var chatTarget = Optional.ofNullable(server.getPublicChannelWebhook())
-                .or(() -> publicChannelId.flatMap(adapter::createWebhook))
+        final var publicChannel = Optional.ofNullable(server.getPublicChannelId());
+        this.chatRelay = Optional.ofNullable(server.getPublicChannelWebhook())
+                .or(() -> publicChannel.flatMap(adapter::createWebhook))
                 .map(url -> new WebhookClientBuilder(url).buildJDA())
                 .map(adapter::minecraftChatTemplate)
-                .or(() -> publicChannelId.map(adapter::minecraftChatTemplate));
+                .or(() -> publicChannel.map(adapter::minecraftChatTemplate))
+                .orElseThrow();
+
         final var consoleChannel = Optional.ofNullable(server.getConsoleChannelId());
         final var consoleStream = consoleChannel.map(id -> adapter.channelAsStream(id, srv.getServer().isLessConsoleSpam()));
-        Polyfill.stream(chatTarget.map(target -> srv.filter(e -> DelegateStream.IO.EventKey_Output.equals(e.getKey()))
+
+        Polyfill.stream(
+                // public channel
+                publicChannel.map(id -> adapter.flatMap(MessageReceivedEvent.class)
+                        .filterData(e -> e.getChannel().getIdLong() == id)
+                        .mapData(MessageReceivedEvent::getMessage)
+                        .filterData(msg -> !msg.getAuthor().isBot())
+                        .mapData(msg -> "tell @a §6<%s>§r %s".formatted(
+                                msg.getAuthor().getEffectiveName(),
+                                msg.getContentStripped()))
+                        .subscribeData(srv.getIn()::println)).stream(),
+                Stream.of(srv.filter(e -> DelegateStream.IO.EventKey_Output.equals(e.getKey()))
                                 .mapData(ChatPattern_Vanilla::matcher)
                                 .filterData(Matcher::matches)
                                 .subscribeData(matcher -> {
@@ -51,10 +69,12 @@ public class DiscordConnection extends Container.Base {
                                     var username = matcher.group("username");
                                     var message = matcher.group("message");
                                     var profile = bean(MinecraftProfileRepo.class).get(username);
-                                    target.accept(profile, message);
-                                }))
-                        .stream(),
+                                    chatRelay.accept(profile, message);
+                                })),
+
                 //todo: moderation channel
+
+                // console channel
                 consoleChannel.map(id -> adapter.flatMap(MessageReceivedEvent.class)
                                 .filterData(e -> e.getChannel().getIdLong() == id)
                                 .mapData(MessageReceivedEvent::getMessage)
@@ -63,10 +83,8 @@ public class DiscordConnection extends Container.Base {
                                 .filterData(cmd -> cmd.startsWith(">"))
                                 .peekData(cmd -> consoleStream.ifPresent(out -> out.println(cmd)))
                                 .mapData(cmd -> cmd.substring(1))
-                                .subscribeData(srv.getIn()::println))
-                        .stream(),
-                consoleStream.map(target -> srv.getOe().redirect(target, target))
-                        .stream()
+                                .subscribeData(srv.getIn()::println)).stream(),
+                consoleStream.map(target -> srv.getOe().redirect(target, target)).stream()
         ).forEach(this::addChildren);
     }
 }
