c3e9e7234fcc0ae5d7d425842e906cb63609457d
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2023-08-14 20:54:39 +0200

    improve runscript

--- gitRun.sh
+++ gitRun.sh
@@ -5,8 +5,17 @@ export pidFile="unit.pid"
 echo $$ > $pidFile
 trap 'rm -f $pidFile' EXIT
 
+if [ -z $1 ]; then
+  branch="main"
+else
+  branch="$1"
+fi
+
 function fetch() {
-  git checkout main
+  if git show-ref --verify --quiet "refs/heads/$branch"; then
+    branch="main"
+  fi
+  git checkout "$branch"
   git pull
 }
 
