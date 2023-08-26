6e97eab970b22613b31abf581248052b3b3e045c
burdoto <kaleidox@comroid.org>
2023-08-26 11:58:45 +0200

    Update ServerUptimeEntry.java

--- src/core/main/java/org/comroid/mcsd/core/entity/ServerUptimeEntry.java
+++ src/core/main/java/org/comroid/mcsd/core/entity/ServerUptimeEntry.java
@@ -1,13 +1,9 @@
 package org.comroid.mcsd.core.entity;
 
 import jakarta.persistence.*;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import org.comroid.api.BitmaskAttribute;
-import org.comroid.mcsd.api.model.IStatusMessage;
 import org.comroid.mcsd.api.model.Status;
-import org.comroid.util.Bitmask;
 
 import java.time.Instant;
 
@@ -15,7 +11,7 @@ import java.time.Instant;
 @Setter
 @Entity
 @Table(name = "uptime")
-public class UptimeEntry extends AbstractEntity {
+public class ServerUptimeEntry extends AbstractEntity {
     private @ManyToOne AbstractEntity entity;
     private Instant timestamp = Instant.now();
     private Status status;
--- src/core/main/java/org/comroid/mcsd/core/repo/UptimeRepo.java
+++ src/core/main/java/org/comroid/mcsd/core/repo/UptimeRepo.java
@@ -1,9 +1,9 @@
 package org.comroid.mcsd.core.repo;
 
-import org.comroid.mcsd.core.entity.UptimeEntry;
+import org.comroid.mcsd.core.entity.ServerUptimeEntry;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.UUID;
 
-public interface UptimeRepo extends CrudRepository<UptimeEntry, UUID> {
+public interface UptimeRepo extends CrudRepository<ServerUptimeEntry, UUID> {
 }
