80fef2ca244aff2af9c5b3f995ac7ef072778ff3
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2023-10-01 23:10:01 +0200

    Update gitRun.sh

--- gitRun.sh
+++ gitRun.sh
@@ -12,18 +12,20 @@ else
 fi
 
 function fetch() {
-  prevBranch="$branch"
   if [ -n "$1" ]; then
     echo "Checking out commit: $1"
     git checkout "$1"
-  elif [ "$(git show-ref --verify --quiet "refs/heads/$branch")" ]; then
+    return
+  fi
+  prevBranch="$branch"
+  if [ "$(git show-ref --verify --quiet "refs/heads/$branch")" ]; then
     branch="main"
     echo "Checking out default branch: $branch"
     git checkout "$branch"
   fi
-
-  echo "Pulling changes..."
+  echo "Pulling changes from $branch..."
   git pull
+
   branch="$prevBranch"
   echo "Restoring branch: $branch"
 }
