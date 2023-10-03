dd30385f73c5170b8103b30d50284ea095971575
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2023-10-03 19:25:39 +0200

    fix death message pattern
    also imrove logging

--- src/core/main/java/org/comroid/mcsd/core/module/player/ConsolePlayerEventModule.java
+++ src/core/main/java/org/comroid/mcsd/core/module/player/ConsolePlayerEventModule.java
@@ -72,7 +72,7 @@ public class ConsolePlayerEventModule extends ServerModule {
     public static final Pattern DeathPatternBase = Pattern.compile("^%1\\$s (?<message>[\\w\\s%$]+)$");
     public static final @Language("RegExp") String DeathPatternScheme = ".*INFO]: (?<username>[\\S\\w-_]+) (?<message>%s)\\r?\\n?.*";
     @SuppressWarnings("RegExpDuplicateCharacterInClass")
-    public static final @Language("RegExp") String CleanWord_Spaced = "\\[?([\\\\s\\\\w\\-_]+)]?";
+    public static final @Language("RegExp") String CleanWord_Spaced = "\\\\[?([\\\\s\\\\w\\\\-_]+)]?";
     public static final List<Pattern> DeathMessagePatterns = new ArrayList<>();
 
     final AtomicReference<TickerMessage> lastTickerMessage = new AtomicReference<>(new TickerMessage(now(), -1));
@@ -103,6 +103,7 @@ public class ConsolePlayerEventModule extends ServerModule {
                     })
                     .map(Pattern::compile)
                     .forEach(DeathMessagePatterns::add);
+            log.info(DeathMessagePatterns.size()+" death messages registered");
         } catch (Throwable t) {
             log.log(Level.INFO, "Unable to fetch death messages; disabling support for it", t);
         }
