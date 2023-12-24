7df0f2e19277a84c18fedd7d333dc61d499cead4
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2023-12-24 14:20:41 +0100

    plugin

--- src/spigot/main/java/org/comroid/mcsd/spigot/MCSD_Spigot.java
+++ src/spigot/main/java/org/comroid/mcsd/spigot/MCSD_Spigot.java
@@ -4,10 +4,17 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MCSD_Spigot extends JavaPlugin {
+    @Override
+    public void onLoad() {
+        var config = getConfig();
+        initConfigDefaults(config);
+    }
+
     private void initConfigDefaults(FileConfiguration config) {
         config.addDefault("mcsd.hubBaseUrl", "<mcsd hub base url>");
-        config.addDefault("mcsd.agent.serverId", "<mcsd ServerAgent id>");
-        config.addDefault("mcsd.agent.token", "<mcsd ServerAgent token>");
+        config.addDefault("mcsd.agent.id", "<mcsd agent id>");
+        config.addDefault("mcsd.agent.token", "<mcsd agent token>");
+        config.addDefault("mcsd.agent.serverId", "<mcsd server id>");
 
         saveConfig();
     }
