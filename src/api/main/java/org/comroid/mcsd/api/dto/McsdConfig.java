e637dce75229d30b3a54cbfdc262d83b4d399b1d
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2024-02-04 20:16:24 +0100

    Update McsdConfig.java

--- src/api/main/java/org/comroid/mcsd/api/dto/McsdConfig.java
+++ src/api/main/java/org/comroid/mcsd/api/dto/McsdConfig.java
@@ -9,6 +9,7 @@ import java.util.List;
 public class McsdConfig {
     @Nullable String hubBaseUrl;
     @Nullable String discordToken;
+    @Nullable String rabbitUrl;
     DBInfo database;
     List<OAuth2Info> oAuth;
     @Nullable AgentInfo agent;
