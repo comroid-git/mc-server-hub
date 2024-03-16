faaa1bdc15e0aab4dad127f67b380008038e17a3
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2024-03-16 17:56:27 +0100

    use separate Rx and Tx modules

--- src/core/main/java/org/comroid/mcsd/core/entity/module/remote/rabbit/RabbitRxModulePrototype.java
+++ src/core/main/java/org/comroid/mcsd/core/entity/module/remote/rabbit/RabbitRxModulePrototype.java
@@ -2,5 +2,5 @@ package org.comroid.mcsd.core.entity.module.remote.rabbit;
 
 import org.comroid.mcsd.core.entity.module.console.ConsoleModulePrototype;
 
-public class RabbitModulePrototype extends ConsoleModulePrototype {
+public class RabbitRxModulePrototype extends ConsoleModulePrototype {
 }
