164c649628494ba9c3341a7d471fb6f93bd9bcee
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2024-03-23 17:16:16 +0100

    add console output forwarder

--- src/spigot/main/java/org/comroid/mcsd/spigot/MCSD_Spigot.java
+++ src/spigot/main/java/org/comroid/mcsd/spigot/MCSD_Spigot.java
@@ -1,5 +1,6 @@
 package org.comroid.mcsd.spigot;
 
+import lombok.Getter;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.comroid.api.Polyfill;
@@ -13,35 +14,36 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.UUID;
 import java.util.logging.Level;
+import java.util.logging.Logger;
 
+@Getter
 public final class MCSD_Spigot extends JavaPlugin {
     public static final String DefaultHubBaseUrl = "https://mc.comroid.org";
     public static final String DefaultRabbitUri = "amqp://anonymous:anonymous@rabbitmq.comroid.org:5672/mcsd";
+    public static final String DefaultConsoleLevel = Level.INFO.getName();
     public static final String ExchangeConsole = "mcsd.module.console";
     public static final String ExchangePlayerEvent = "mcsd.module.player";
     public static final String RouteConsoleInputBase = "input.%s";
     public static final String RouteConsoleOutputBase = "output.%s";
     public static final String RoutePlayerEventBase = "event.%s.%s";
 
-    private EventManager eventManager;
-    private FileConfiguration config;
-    private Command.Manager cmdr;
-    private Rabbit.Exchange console;
-    private Rabbit.Exchange players;
+    EventManager eventManager;
+    FileConfiguration config;
+    Command.Manager cmdr;
+    UUID serverId;
+    Level consoleLevel;
+    Rabbit.Exchange console;
+    Rabbit.Exchange players;
 
     @Override
     public void onLoad() {
+        // config
         this.config = getConfig();
         initConfigDefaults();
 
-        var rabbit = Wrap.of(config.get("mcsd.rabbitMqUri", DefaultRabbitUri))
-                .map(String::valueOf)
-                .flatMap(Rabbit::of)
-                .requireNonNull("Unable to initialize RabbitMQ connection");
-        this.console = rabbit.exchange(ExchangeConsole);
-        this.players = rabbit.exchange(ExchangePlayerEvent);
-
+        // plugin commands
         this.cmdr = new Command.Manager();
         cmdr.new Adapter$Spigot(this);
         cmdr.register(this);
@@ -50,14 +52,30 @@ public final class MCSD_Spigot extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        // convert config
+        this.serverId = UUID.fromString(Objects.requireNonNull(config.getString("mcsd.server.id"), "Server ID not configured"));
+        this.consoleLevel = Level.parse(config.getString("mcsd.consoleLevel", DefaultConsoleLevel));
+
+        // send hello to hub
         req(REST.Method.GET, "/agent/hello/" + config.getString("mcsd.agent.id"))
                 .execute()
                 .thenApply(REST.Response::validate2xxOK)
                 .thenRun(() -> getLogger().info("Ping to Hub succeeded"))
                 .exceptionally(Polyfill.exceptionLogger(getLogger(), Level.WARNING, Level.FINE, "Could not reach Hub @ " + config.getString("mcsd.hubBaseUrl")));
 
-        this.eventManager = new EventManager(this);
+        // rabbitmq
+        var rabbit = Wrap.of(config.get("mcsd.rabbitMqUri", DefaultRabbitUri))
+                .map(String::valueOf)
+                .flatMap(Rabbit::of)
+                .requireNonNull("Unable to initialize RabbitMQ connection");
+        this.console = rabbit.exchange(ExchangeConsole);
+        this.players = rabbit.exchange(ExchangePlayerEvent);
 
+        // logger configuration
+        Logger.getGlobal().addHandler(new LoggerHandler(this));
+
+        // bukkit events
+        this.eventManager = new EventManager(this);
         getServer().getPluginManager().registerEvents(eventManager, this);
     }
 
@@ -96,6 +114,7 @@ public final class MCSD_Spigot extends JavaPlugin {
     private void initConfigDefaults() {
         initConfigAttribute("mcsd.hubBaseUrl", DefaultHubBaseUrl, "MCSD Hub Base URL");
         initConfigAttribute("mcsd.rabbitMqUri", DefaultRabbitUri, "MCSD RabbitMQ URI");
+        initConfigAttribute("mcsd.consoleLevel", DefaultConsoleLevel, "Console Minimum Log Level");
         initConfigAttribute("mcsd.server.id", null, "MCSD Server ID");
 
         saveConfig();
