be0cb987486f9aa9b1d70b982825c372009a76e8
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2024-03-23 17:01:05 +0100

    add basic rabbit fields

--- src/spigot/main/java/org/comroid/mcsd/spigot/MCSD_Spigot.java
+++ src/spigot/main/java/org/comroid/mcsd/spigot/MCSD_Spigot.java
@@ -3,8 +3,10 @@ package org.comroid.mcsd.spigot;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.comroid.api.Polyfill;
+import org.comroid.api.func.ext.Wrap;
 import org.comroid.api.func.util.Command;
 import org.comroid.api.net.REST;
+import org.comroid.api.net.Rabbit;
 import org.comroid.mcsd.api.dto.comm.PlayerEvent;
 import org.comroid.util.PathUtil;
 import org.jetbrains.annotations.Nullable;
@@ -16,14 +18,29 @@ import java.util.logging.Level;
 public final class MCSD_Spigot extends JavaPlugin {
     public static final String DefaultHubBaseUrl = "https://mc.comroid.org";
     public static final String DefaultRabbitUri = "amqp://anonymous:anonymous@rabbitmq.comroid.org:5672/mcsd";
+    public static final String ExchangeConsole = "mcsd.module.console";
+    public static final String ExchangePlayerEvent = "mcsd.module.player";
+    public static final String RouteConsoleInputBase = "input.%s";
+    public static final String RouteConsoleOutputBase = "output.%s";
+    public static final String RoutePlayerEventBase = "event.%s.%s";
+
     private EventManager eventManager;
     private FileConfiguration config;
     private Command.Manager cmdr;
+    private Rabbit.Exchange console;
+    private Rabbit.Exchange players;
 
     @Override
     public void onLoad() {
         this.config = getConfig();
         initConfigDefaults();
+
+        var rabbit = Wrap.of(config.get("mcsd.rabbitMqUri", DefaultRabbitUri))
+                .map(String::valueOf)
+                .flatMap(Rabbit::of)
+                .requireNonNull("Unable to initialize RabbitMQ connection");
+        this.console = rabbit.exchange(ExchangeConsole);
+        this.players = rabbit.exchange(ExchangePlayerEvent);
 
         this.cmdr = new Command.Manager();
         cmdr.new Adapter$Spigot(this);
@@ -44,16 +61,18 @@ public final class MCSD_Spigot extends JavaPlugin {
         getServer().getPluginManager().registerEvents(eventManager, this);
     }
 
+    @Override
+    public void onDisable() {
+        eventManager.close();
+    }
+
+    public void forward(PlayerEvent event) {}
+
     @Command(permission = "mcsd.reload")
     public String reload() {
         onDisable();
         onEnable();
         return "Reloaded!";
-    }
-
-    @Override
-    public void onDisable() {
-        eventManager.close();
     }
 
     private String hub(String path) {
