01a8bba3ad02a46e12e91dfd9420e7008e5699a5
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2024-04-01 19:32:25 +0200

    use a log4j level filter

--- src/api/main/java/org/comroid/mcsd/api/log/RabbitConsoleAppender.java
+++ src/api/main/java/org/comroid/mcsd/api/log/RabbitConsoleAppender.java
@@ -3,27 +3,31 @@ package org.comroid.mcsd.api.log;
 import lombok.Value;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Filter;
 import org.apache.logging.log4j.core.LogEvent;
 import org.apache.logging.log4j.core.Logger;
 import org.apache.logging.log4j.core.appender.AbstractAppender;
 import org.apache.logging.log4j.core.config.plugins.Plugin;
+import org.apache.logging.log4j.core.filter.LevelRangeFilter;
 import org.comroid.api.net.Rabbit;
 import org.comroid.mcsd.api.dto.comm.ConsoleData;
 
 import java.util.Calendar;
 
+import static org.apache.logging.log4j.Level.*;
+import static org.apache.logging.log4j.core.Filter.Result.*;
 import static org.comroid.mcsd.api.dto.comm.ConsoleData.output;
 
 @Value
 @Plugin(name = "MCSD Console Relay", category = "Core", elementType = "appender", printObject = true)
 public class RabbitConsoleAppender extends AbstractAppender {
     Rabbit.Exchange.Route<ConsoleData> route;
-    Level level;
 
     public RabbitConsoleAppender(Rabbit.Exchange.Route<ConsoleData> route, Level level) {
-        super("MCSD Console Appender", null, null, false, null);
+        super("MCSD Console Appender",
+                LevelRangeFilter.createFilter(OFF, level, ACCEPT, DENY),
+                null, false, null);
         this.route = route;
-        this.level = level;
 
         ((Logger) LogManager.getRootLogger()).addAppender(this);
     }
@@ -31,8 +35,6 @@ public class RabbitConsoleAppender extends AbstractAppender {
     @Override
     public void append(LogEvent event) {
         try {
-            if (event.getLevel().intLevel() > level.intLevel())
-                return;
             var calendar = Calendar.getInstance();
             calendar.setTimeInMillis(event.getTimeMillis());
             var str = "[%tT %s] [%s]: %s\n".formatted(
